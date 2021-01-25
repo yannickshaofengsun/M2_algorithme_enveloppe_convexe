7329f1801e46058ea7f72bc23542f6ff7e913639
yannickshaofengsun <74181931+yannickshaofengsun@users.noreply.github.com>
2021-01-25 20:53:51 +0100

    Update temps_algo.R

--- R/temps_algo.R
+++ R/temps_algo.R
@@ -1,41 +1,41 @@
 temps_javis_rcpp <- function(l){
   start_time <- Sys.time()
-  env_convex_javis_cpp(t(M))
+  env_convex_javis_cpp(t(l))
   end_time <- Sys.time()
   time = end_time - start_time
   return(time)}
   
 temps_javis_r <- function(l){
   start_time <- Sys.time()
-  env_conv(t(M))
+  env_conv(t(l))
   end_time <- Sys.time()
   time = end_time - start_time
   return(time)}
 
 temps_naive_rcpp <- function(l){
   start_time <- Sys.time()
-  naif_convex_rcpp(M)
+  naif_convex_rcpp(l)
   end_time <- Sys.time()
   time = end_time - start_time
   return(time)}
 
 temps_naive<- function(l){
   start_time <- Sys.time()
-  env_convex_naif(M)
+  env_convex_naif(l)
   end_time <- Sys.time()
   time = end_time - start_time
   return(time)}
 
 temps_graham_rcpp <- function(l){
   start_time <- Sys.time()
-  parcours_graham_cpp(M)
+  parcours_graham_cpp(l)
   end_time <- Sys.time()
   time = end_time - start_time
   return(time)}
 
 temps_graham <- function(l){
   start_time <- Sys.time()
-  parcours_graham(M)
+  parcours_graham(l)
   end_time <- Sys.time()
   time = end_time - start_time
   return(time)}
